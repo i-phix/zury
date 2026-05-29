d60efafe80b586e37a041ab39528cf19fc195384
Chester <iphix09@gmail.com>
2026-05-29 10:01:46 +0300

    connection to db config

--- server/src/database/connection.js
+++ server/src/database/connection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export async function connectDB() {
   try {
-    const uri = process.env.MONGO_URI||"mongodb://localhost:27017/zuri"
+    const uri = process.env.MONGO_URI
     if (!uri) throw new Error('MONGO_URI is not defined')
 
     await mongoose.connect(uri, {
